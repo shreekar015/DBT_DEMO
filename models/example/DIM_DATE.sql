db93289cdeec019a66d7a9cbc8f765166cfae4ac
Shreekar Patra <shreekar.patra@smarterp.com>
2026-07-08 19:36:23 +0000

    daily_weather model created

--- models/example/DIM_DATE.sql
+++ models/example/DIM_DATE.sql
@@ -3,20 +3,9 @@ select
 to_timestamp(STARTED_AT)  as STARTED_AT,
 date(STARTED_AT) as date_STARTED_AT,
 hour(to_timestamp(STARTED_AT))  as hour_STARTED_AT,
-case
-when  dayname(to_timestamp(STARTED_AT)) in ('Sat','Sun')
-then 'WEEKEND'
-ELSE 'BUSINESSDAY'
-END AS DAY_TYPE,
 
-CASE 
-WHEN MONTH(to_timestamp(STARTED_AT)) IN (12,1,2)
-THEN 'WINTER'
-WHEN MONTH(to_timestamp(STARTED_AT)) IN (3,4,5) THEN 'SPRING'
-WHEN MONTH(to_timestamp(STARTED_AT)) IN (6,7,8) THEN 'SUMMER'
-ELSE 'AUTUMN'
-END AS STATION_OF_YEAR
-
+{{day_type('STARTED_AT')}} AS DAY_TYPE,
+{{get_season('STARTED_AT')}} AS STATION_OF_YEAR
 
 from
 {{ source('demo', 'bike') }}
