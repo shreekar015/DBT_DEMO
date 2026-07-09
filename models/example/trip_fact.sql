e47151fa539b7b4aef26daf7485f88952384a700
Shreekar Patra <shreekar.patra@smarterp.com>
2026-07-09 19:06:34 +0000

    trip fact model

---
+++ models/example/trip_fact.sql
@@ -0,0 +1,16 @@
+with Trips as (
+    select 
+    RIDE_ID,
+    RIDEABLE_TYPE,
+    date(to_timestamp(started_at)) as trip_date,
+    START_STATIO_ID,
+    END_STATION_NAME,
+    MEMBER_CSUAL as MEMBER_CASUAL,
+    timestampdiff(second, to_timestamp(started_at), to_timestamp(ended_at)) as trip_duration_seconds
+    from
+    {{ source('demo', 'bike') }}
+    where
+    ride_id != 'ride_id'
+)
+
+select * from Trips
